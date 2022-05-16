b2f30e6ca0105925635b1f159e6b450d51ca1f49
NhoJONGHYUN <shgus79@ajou.ac.kr>
2022-05-16 17:31:11 +0900

    host 바꿈

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("mzd.main:app", host="localhost", port=80, reload=True)
+    uvicorn.run("mzd.main:app", host="0.0.0.0", port=80, reload=True)
