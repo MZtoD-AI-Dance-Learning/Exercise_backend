2d1d2c87977dda2a9dec957d3c090e980b0fb319
JongHyunNho <61325655+JongHyunNho@users.noreply.github.com>
2022-05-16 15:24:19 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="localhost", port=80, reload=True)
+    uvicorn.run("mzd.main:app", host="localhost", port=80, reload=True)
